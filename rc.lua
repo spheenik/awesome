3953f90c922298289be2ac4d80c4ebe3116af029
Martin Schrodt <martin@schrodt.org>
2024-09-28 16:29:43 +0300

    fix: awful.util.spawn -> awful.spawn

--- rc.lua
+++ rc.lua
@@ -296,24 +296,24 @@ globalkeys = awful.util.table.join(
 
     -- Volume Control
     awful.key({ }, "XF86AudioRaiseVolume", function ()
-        awful.util.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ 0", false)
-        awful.util.spawn("pactl -- set-sink-volume @DEFAULT_SINK@ +4%", false)
-        awful.util.spawn("pactl -- play-sample volumewav", false)
+        awful.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ 0", false)
+        awful.spawn("pactl -- set-sink-volume @DEFAULT_SINK@ +4%", false)
+        awful.spawn("pactl -- play-sample volumewav", false)
     end),
     awful.key({ }, "XF86AudioLowerVolume", function ()
-        awful.util.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ 0", false)
-        awful.util.spawn("pactl -- set-sink-volume @DEFAULT_SINK@ -4%", false)
-        awful.util.spawn("pactl -- play-sample volumewav", false)
+        awful.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ 0", false)
+        awful.spawn("pactl -- set-sink-volume @DEFAULT_SINK@ -4%", false)
+        awful.spawn("pactl -- play-sample volumewav", false)
     end),
     awful.key({ }, "XF86AudioMute", function()
-        awful.util.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ toggle", false)
-        awful.util.spawn("pactl -- play-sample volumewav", false)
+        awful.spawn("pactl -- set-sink-mute @DEFAULT_SINK@ toggle", false)
+        awful.spawn("pactl -- play-sample volumewav", false)
     end),
     awful.key({ }, "XF86MonBrightnessUp", function()
-        awful.util.spawn("light -A 10", false)
+        awful.spawn("light -A 10", false)
     end),
     awful.key({ }, "XF86MonBrightnessDown", function()
-        awful.util.spawn("light -U 10", false)
+        awful.spawn("light -U 10", false)
     end)
 )
 
